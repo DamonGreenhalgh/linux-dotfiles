b9e8bc12ae28d4f2af60884d2c6fca4548206f93
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2025-12-19 21:09:56 +1300

    add gacha timers feature to waybar

--- applytheme.sh
+++ applytheme.sh
@@ -8,6 +8,8 @@ configPath=/home/damon/.config    # The path to your app configs, usually ~/.con
 currentPath=/home/damon/Documents/rice
 
 # Copy over and replace files
+cp -r $currentPath/scripts/. $configPath/scripts/
+cp -r $currentPath/assets/. $configPath/assets/
 cp -r $currentPath/waybar/. $configPath/waybar/
 cp -r $currentPath/eww/. $configPath/eww/
 cp -r $currentPath/fastfetch/. $configPath/fastfetch/
@@ -16,6 +18,7 @@ cp -r $currentPath/kitty/. $configPath/kitty/
 cp -r $currentPath/mako/. $configPath/mako/
 cp -r $currentPath/Code/. $configPath/Code/
 cp -r $currentPath/wofi/. $configPath/wofi/
+cp -r $currentPath/quickshell/. $configPath/quickshell/
 cp -r $currentPath/firefox/. /home/damon/.mozilla/firefox/u0oe936c.default-release
 cp $currentPath/bashrc /home/damon/.bashrc
 
---
+++ quickshell/default/shell.qml
@@ -0,0 +1,12 @@
+import Quickshell 1.0
+Shell {
+    Panel {
+        anchors.top: parent.top
+        width: parent.width
+        height: 30
+        Text {
+            text: "Hello Quickshell"
+            anchors.centerIn: parent
+        }
+    }
+}
